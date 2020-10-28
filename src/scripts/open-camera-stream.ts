e59ff9252b8d0560e4f5ce0d6bf9f94ffad04e9e
schmelczerandras <schmelczer.andras@simonyi.bme.hu>
2020-10-28 08:06:48 +0000

    Prefer backfacing camera

--- src/scripts/open-camera-stream.ts
+++ src/scripts/open-camera-stream.ts
@@ -1,4 +1,11 @@
-export const openCameraStream = async (): Promise<MediaStream> =>
-  await navigator.mediaDevices.getUserMedia({
-    video: true,
-  });
+export const openCameraStream = async (): Promise<MediaStream> => {
+  try {
+    return await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "environment" },
+    });
+  } catch {
+    return await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+  }
+};
