c60d30a932a9cd221e02bd46f64acd9495ed8269
radl97 <radl97@users.noreply.github.com>
2020-12-03 22:35:49 +0100

    Use circular buffer

--- src/scripts/perspective-reverse/bounding-box-storage.ts
+++ src/scripts/perspective-reverse/bounding-box-storage.ts
@@ -3,10 +3,16 @@ import { BoundingBox } from '../bounding-box/bounding-box';
 // Store bounding boxes for finding out the perspective
 export class BoundingBoxStorage {
     private storage : BoundingBox[] = [];
+    private lastIdx = 0;
 
     public registerBoundingBox(a: BoundingBox): void {
-        if (this.storage.length < 1000) {
+        if (this.storage.length < 1024) {
             this.storage.push(a);
+        } else {
+            // length is 1024, overwrite as a circular buffer
+            // idea cretited to Andrew
+            this.storage[this.lastIdx] = a;
+            this.lastIdx = (this.lastIdx+1)%1024;
         }
     }
 
