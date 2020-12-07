46d80b3714cee69df79af29c6536a83cb589de3c
schmelczerandras <schmelczer.andras@simonyi.bme.hu>
2020-12-07 12:02:19 +0000

    Format file

--- src/scripts/orientation-provider/orientation-provider.ts
+++ src/scripts/orientation-provider/orientation-provider.ts
@@ -1,29 +1,31 @@
 export class OrientationProvider {
+  private orientation: number | null;
+  constructor() {
+    this.orientation = null;
+    window.addEventListener(
+      "deviceorientation",
+      (event) => {
+        //const alpha    = event.alpha; // this is for ARs
+        const beta = event.beta;
+        const gamma = event.gamma;
+        if (beta == null || gamma == null) {
+          return;
+        }
+        // to support both orientations, the important data might be either beta or gamma
 
-    private orientation : number | null;
-    constructor() {
-        this.orientation = null;
-        window.addEventListener("deviceorientation", event => {
-            //const alpha    = event.alpha; // this is for ARs
-            const beta     = event.beta;
-            const gamma    = event.gamma;
-            if (beta == null || gamma == null) {
-                return;
-            }
-            // to support both orientations, the important data might be either beta or gamma
+        // if one is closer to 0° or 90°, then choose the other
+        const beta2 = Math.abs(Math.min(beta, 90 - beta));
+        const gamma2 = Math.abs(Math.min(gamma, 90 - gamma));
+        this.orientation = (-Math.PI / 180) * Math.max(beta2, gamma2);
+      },
+      true
+    );
+  }
 
-            // if one is closer to 0° or 90°, then choose the other
-            const beta2 = Math.abs(Math.min(beta, 90-beta));
-            const gamma2 = Math.abs(Math.min(gamma, 90-gamma));
-            this.orientation = -Math.PI/180*Math.max(beta2, gamma2);
-        }, true);
-    }
-
-    /**
-     * Returns the orientation (yaw) of the phone or null, if it cannot be provided
-     */
-    getOrientation() : number | null {
-        return this.orientation;
-    }
-
+  /**
+   * Returns the orientation (yaw) of the phone or null, if it cannot be provided
+   */
+  getOrientation(): number | null {
+    return this.orientation;
+  }
 }
